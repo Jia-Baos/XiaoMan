43fddaebbcbec0688d0662bc6c9585041d8e8f22
Jia-Baos <18383827268@163.com>
2022-04-14 08:18:18 +0800

    2022/04/14 08:17 add 1DResNet 1DSENet

--- 1DSENet.py
+++ 1DSENet.py
@@ -8,31 +8,36 @@ class SELayer(nn.Module):
     the Squeeze and Excitation realizes the interaction of features from different channels
     the activation func of Sigmoid realizes the flexibility of weights, which means several channels can embrace the high weights at the same time
     """
-    def __init__(self, channel=3, reduction=4):
+
+    def __init__(self, in_channels=8, reduction=4):
         super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
         self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
+            nn.Linear(in_channels, in_channels // reduction, bias=False),
             nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Linear(in_channels // reduction, in_channels, bias=False),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        print("avg_pool: ", y.size())
-        y = self.fc(y).view(b, c, 1, 1)
-        print("Squeeze and Excitation: ", y.size())
-        y = y.expand_as(x)
-        print("expand_as x: ", y.size())
+        y = self.avg_pool(x)
+        y = y.reshape(x.shape[0], x.shape[1])
+        print("avg_pool's size: ", y.size())
+        print("avg_pool: ", y)
+
+        y = self.fc(y)
+        y = y.reshape(x.shape[0], x.shape[1], 1)
+        print("Squeeze and Excitation's size: ", y.size())
+        print("Squeeze and Excitation: ", y)
+
         return x * y
 
 
 if __name__ == '__main__':
-    x = torch.rand((4, 3, 4, 4), dtype=torch.float)
+    x = torch.rand((12, 8, 100), dtype=torch.float)
     print("input's size: ", x.size())
-    model = SELayer()
+    # print("input: ", x)
+    model = SELayer(in_channels=8,reduction=4)
     y = model(x)
-    print(y.size())
     print("output's size: ", y.size())
+    # print("output: ", y)
